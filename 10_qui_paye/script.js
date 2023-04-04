624b8c5d9ea02a2f2e129862f310001c9e876224
enzo-leonard <enzoleonard96@gmail.com>
2023-04-04 12:43:54 +0200

    xxs

--- 10_qui_paye/script.js
+++ 10_qui_paye/script.js
@@ -1,8 +1,31 @@
 // ajouter 2 prénoms 
 
+
 // Lorsqu'un clique sur le bouton "qui paye ?" on affiche le premier prénom de la liste
+const perdant = document.querySelector("#perdant")
+const btn = document.querySelector('#bouton')
+const playersHTML = document.querySelector('#players')
+let players = playersHTML.value.split(' ')
 
 
+playersHTML.addEventListener('keyup', () => {
+    players = playersHTML.value.split('\n')
+    console.log(players)
+})
+
+
+console.log(players)
+
+const randomNumber = (min, max)  => { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function quiPaie(players) {
+    const nbPlayer = randomNumber(0,(players.length-1))
+    perdant.innerHTML= players[nbPlayer]
+}
+
+btn.addEventListener('click',() => quiPaie(players))
 // On trouve un nombré aléatoire entre 0 et 1
 
 
