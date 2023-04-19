3e18dff7fcad5b2d321852a3f39ad8abf07e550f
enzo-leonard <enzoleonard96@gmail.com>
2023-04-19 17:56:35 +0200

    burger

---
+++ 12_burger/script.js
@@ -0,0 +1,31 @@
+import {burgerList} from './data.js'
+
+
+// Voici la liste des burgers (le fichier se trouve dans le dossier data.js)
+console.log('burgerList', burgerList)
+
+
+// Etape 1
+// Pour chacun des burgers affiche le nom dans la console
+
+// Etape 2
+// Dans #burger-container afficher le nom des burgers
+
+// Etape 3 
+// Ajouter la description et le prix 
+
+// Etape 4 
+// Afficher l'image du burger
+
+// Etape 5
+// Ajouter des nouveaux burgers dans le fichier data.js
+
+// Etape 6 
+// Ajouter votre site sur github page et ajouter le lien dans le cours 
+
+// Etape 7 (Bonus)
+// Rendre vos site le plus attractif possible en changeant le style
+// Vous pouvez remplacer les burgers par d'autre élements (ex: des films, des livres, des jeux vidéos, des personnages, des animaux, etc...)
+
+// Etape 8 (Bonus)
+// Afficher la liste des ingrédients
