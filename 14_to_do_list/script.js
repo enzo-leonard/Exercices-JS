355f75343b6e9beaab26c59b1bde1aea13127a26
enzo-leonard <enzoleonard96@gmail.com>
2023-04-25 12:14:16 +0200

    de

--- 14_to_do_list/script.js
+++ 14_to_do_list/script.js
@@ -1,26 +1,33 @@
 const defaultTodo = [
-    {done: true, name: "Coucou"},
-    {done: true, name: "Coucou"},
+    {done: true, name: "Tache 1"},
+    {done: true, name: "Tache 2"},
 ]
 
-const displayTodos = (listTodo) => {
-
-}
-
-
 // #1 selectionner le bouton avec l'id #add, le container #container-list, le input #new
-
+const addBtn = document.querySelector('#add')
+const container = document.querySelector('#container-list')
+const input = document.querySelector('#new')
 
 // #2 Afficher les todos dans le container avec une fonction displayTodos
-
-
+const displayTodos = (list) => {
+    container.innerHTML = ""
+    list.forEach(task => {
+        container.innerHTML += `<div class="p-2 bg-slate-700 rounded mt-2">${task.name}</div>`
+    })
+}
+displayTodos(defaultTodo)
 // #3 Ajouter un event listener sur le bouton pour ajouter un todo
-
+addBtn.addEventListener('click', () => {
+    const newTask = {name: input.value, done: false}
+    input.value = ""
+    defaultTodo.push(newTask)
+    displayTodos(defaultTodo)
+})
 
 // BONUS
 
 // #5 Faire un filtre 
-
 // #6 Suprimer un éléments
-
 // #4 Cocher les checkbox pour marquer un todo comme fait
+
+// #Arthur Eliot -- Hugo - Anton 
